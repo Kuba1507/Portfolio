773eebd2fe1fc7247e64e52d3c252ae96b30a54c
Kuba1507 <kuba.romanski@onet.com.pl>
2024-02-26 21:37:24 +0100

    animated mobile menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,23 @@
 const burgerBtn = document.querySelector('.hamburger')
 const navigation = document.querySelector('.nav')
 const navList = document.querySelector('.nav__menu')
+const navItems = document.querySelectorAll('.nav__menu-item-link')
 
 const handleNav = () => {
 	burgerBtn.classList.toggle('is-active')
 	navigation.classList.toggle('show-nav')
-	// navList.classList.toggle('show-menu')
+
+	handleNavItemsAnimation()
+}
+
+const handleNavItemsAnimation = () => {
+	let delayTime = 0
+
+	navItems.forEach(item => {
+		item.classList.toggle('nav-items-animation')
+		item.style.animationDelay = '.' + delayTime + 's'
+		delayTime++
+	})
 }
 
 burgerBtn.addEventListener('click', handleNav)
