198268b173da957cf57a1881dc50af849ce48b07
Kuba1507 <kuba.romanski@onet.com.pl>
2024-02-26 14:10:45 +0100

    made navigation responsive

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
 const burgerBtn = document.querySelector(".burger-btn");
+const navigation = document.querySelector(".nav");
+const navItems = document.querySelectorAll(".nav__item");
+
+const handleNav = () => {
+	navigation.classList.toggle("show-nav");
+
+	navItems.forEach((item) => {
+		item.classList.toggle("show-nav-items");
+	});
+};
+
+burgerBtn.addEventListener("click", handleNav);
