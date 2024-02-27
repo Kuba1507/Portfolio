109b8b710fb8d3aa4eda10d11f1f5c42eabcae7d
Kuba1507 <kuba.romanski@onet.com.pl>
2024-02-27 09:53:05 +0100

    scripted nav shadow and added cta button

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,31 @@
-const burgerBtn = document.querySelector('.hamburger')
-const navigation = document.querySelector('.nav')
-const navList = document.querySelector('.nav__menu')
-const navItems = document.querySelectorAll('.nav__menu-item-link')
+const body = document.querySelector("body");
+const burgerBtn = document.querySelector(".hamburger");
+const navigation = document.querySelector(".nav");
+const navList = document.querySelector(".nav__menu");
+const navItems = document.querySelectorAll(".nav__menu-item-link");
 
 const handleNav = () => {
-	burgerBtn.classList.toggle('is-active')
-	navigation.classList.toggle('show-nav')
+	burgerBtn.classList.toggle("is-active");
+	navigation.classList.toggle("show-nav");
 
-	handleNavItemsAnimation()
-}
+	handleNavItemsAnimation();
+	blockScroll();
+};
 
 const handleNavItemsAnimation = () => {
-	let delayTime = 0
+	let delayTime = 0;
 
-	navItems.forEach(item => {
-		item.classList.toggle('nav-items-animation')
-		item.style.animationDelay = '.' + delayTime + 's'
-		delayTime++
-	})
-}
+	navItems.forEach((item) => {
+		item.classList.toggle("nav-items-animation");
+		item.style.animationDelay = "." + delayTime + "s";
+		delayTime++;
+	});
 
-burgerBtn.addEventListener('click', handleNav)
+	navigation.classList.toggle("navigation-shadow");
+};
+
+const blockScroll = () => {
+	body.classList.add("block-scroll");
+};
+
+burgerBtn.addEventListener("click", handleNav);
